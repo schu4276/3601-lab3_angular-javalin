e842b896089e38ed4a866b58d5a0c2f5e46bb3aa
Paul Friederichsen <floogulinc@gmail.com>
2021-02-14 21:02:59 -0600

    Switch from Protractor to Cypress
    This copies over much of the work on Cypress from https://github.com/UMM-CSci-3601/3601-iteration-template/pull/119

---
+++ client/cypress/integration/app.spec.ts
@@ -0,0 +1,48 @@
+import { AppPage } from '../support/app.po';
+
+const page = new AppPage();
+
+describe('App', () => {
+  beforeEach(() => page.navigateTo());
+
+  it('Should load', () => {
+    //page.navigateTo();
+    cy.document().should('exist');
+  });
+
+  it('Should have the correct title', () => {
+    page.getAppTitle().should('contain', 'CSCI 3601 Lab 3');
+  });
+
+  describe('Sidenav', () => {
+    it('Should be invisible by default', () => {
+      // Before clicking on the button, the sidenav should be hidden
+      page.getSidenav()
+        .should('be.hidden')
+        .and('not.be.visible');
+    });
+
+    it('Should be openable by clicking the sidenav button', () => {
+      page.getSidenavButton().click();
+
+      page.getSidenav()
+        .should('not.be.hidden')
+        .and('be.visible');
+    });
+
+    it('Should have a working navigation to "Users"', () => {
+      page.getSidenavButton().click();
+      page.getSidenav();
+      page.getNavLink('Users').click();
+      cy.url().should('match', /.*\/users$/);
+    });
+
+
+    it('Should have a working navigation to "Home"', () => {
+      page.getSidenavButton().click();
+      page.getNavLink('Home').click();
+      cy.url().should('match', /.*\/$/);
+    });
+  });
+
+});
---
+++ client/cypress/support/user-list.po.ts
@@ -0,0 +1,40 @@
+export class UserListPage {
+  navigateTo() {
+    return cy.visit('/users');
+  }
+
+  getUrl() {
+    return cy.url();
+  }
+
+  getUserTitle() {
+    return cy.get('.user-list-title');
+  }
+
+  getUserCards() {
+    return cy.get('.user-cards-container app-user-card');
+  }
+
+  getUserListItems() {
+    // return element(by.className('user-nav-list')).all(by.className('user-list-item'));
+    return cy.get('.user-nav-list .user-list-item');
+  }
+
+  clickViewProfile(card: Cypress.Chainable<JQuery<HTMLElement>>) {
+    // return card.element(by.buttonText('VIEW PROFILE')).click();
+    return card.contains('button', 'View Profile').click();
+  }
+
+  changeView(viewType: 'card' | 'list') {
+    return cy.get(`#view-type-radio .mat-radio-button[value="${viewType}"]`).click();
+  }
+
+  selectRole(value: string) {
+    // This is messy. I'm not sure that Cypress is working well for us here. Might want to find a better way.
+    return cy.get('#user-role-select').click().get(`mat-option[value="${value}"]`).click();
+  }
+
+  addUserFAB() {
+    return cy.get('.add-user-fab');
+  }
+}
